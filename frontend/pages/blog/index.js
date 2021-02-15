51dc99be5fe8ee693dfc2ae8806197883f8e62de
ben <gibumkang@gmail.com>
2021-02-14 23:53:58 -0800

    Clean up blog section

--- frontend/pages/blog/index.js
+++ frontend/pages/blog/index.js
@@ -2,44 +2,30 @@ import styled from 'styled-components';
 import Layout from '../../components/layout/layout';
 import * as GS from '../../styles/global';
 import Hero from '../../components/hero';
-import { getAllPosts } from '../../lib/ghost';
 import Title from '../../components/title';
 import Bloggy from '../../components/bloggy';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Blog({ posts }) {
 	const pagination = posts.meta.pagination;
-	const router = useRouter();
 	console.log(pagination);
 	return (
-		<>
-			<Layout>
-				<Hero message="Blog" background={`url('/ftgu-academy-header.jpg')`} tags={posts.tags} />
-				<GS.MaxContainer>
-					<GS.YCPadding padding={'5rem'}>
-						<Title title="The Latest" />
-						<Bloggy posts={posts.posts} />
-						<Pagination>
-							<div className="next">
-								<Link href={`/blog/pages/}`}>
-									<a>Next {pagination.next}</a>
-								</Link>
-							</div>
-							<div className="pages">
-								Current: {pagination.page} Total: {pagination.pages}
-							</div>
-							<div className="prev">
-								<Link href={`/blog/pages/`}>
-									<a>Prev {pagination.prev}</a>
-								</Link>
-							</div>
-						</Pagination>
-					</GS.YCPadding>
-				</GS.MaxContainer>
-			</Layout>
-		</>
+		<Layout>
+			<Hero message="Blog" background={`url('/ftgu-academy-header.jpg')`} tags={posts.tags} />
+			<GS.MaxContainer>
+				<GS.YCPadding padding={'5rem'}>
+					<Title title="The Latest" />
+					<Bloggy posts={posts.posts} />
+					<Pagination>
+						<div className="next">Next</div>
+						<div className="pages">
+							Current: {pagination.page} Total: {pagination.pages}
+						</div>
+						<div className="prev">Prev</div>
+					</Pagination>
+				</GS.YCPadding>
+			</GS.MaxContainer>
+		</Layout>
 	);
 }
 
