09cacf00776f365aa1d5a5d37f4b74f7fc08c43d
ben <gibumkang@gmail.com>
2021-02-15 00:10:53 -0800

    getPosts is the solution, making small changes

--- frontend/pages/blog/index.js
+++ frontend/pages/blog/index.js
@@ -4,24 +4,22 @@ import * as GS from '../../styles/global';
 import Hero from '../../components/hero';
 import Title from '../../components/title';
 import Bloggy from '../../components/bloggy';
-import { getAllPosts } from '../../lib/ghost';
+import { getPosts } from '../../lib/ghost';
 import Link from 'next/link';
 
 export default function Blog({ posts }) {
-	const pagination = posts.meta.pagination;
-	console.log(pagination);
 	return (
 		<Layout>
 			<Hero message="Blog" background={`url('/ftgu-academy-header.jpg')`} tags={posts.tags} />
 			<GS.MaxContainer>
 				<GS.YCPadding padding={'5rem'}>
 					<Title title="The Latest" />
-					<Bloggy posts={posts.posts} />
+					<Bloggy posts={posts} />
 					<Pagination>
 						<div className="next">Next</div>
-						<div className="pages">
+						{/* <div className="pages">
 							Current: {pagination.page} Total: {pagination.pages}
-						</div>
+						</div> */}
 						<div className="prev">Prev</div>
 					</Pagination>
 				</GS.YCPadding>
@@ -31,7 +29,7 @@ export default function Blog({ posts }) {
 }
 
 export async function getStaticProps({ context }) {
-	const posts = await getAllPosts();
+	const posts = await getPosts();
 
 	return {
 		props: {
